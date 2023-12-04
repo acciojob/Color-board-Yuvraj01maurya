da7138671f500044cd9580b01c2327c3ae57c699
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-04 23:03:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const container = document.querySelector('.container');
+
+// Function to create 800 square boxes
+function createBoxes() {
+  for (let i = 0; i < 800; i++) {
+    const box = document.createElement('div');
+    box.classList.add('square');
+    container.appendChild(box);
+    box.addEventListener('mouseover', () => {
+      box.style.backgroundColor = getRandomColor();
+      setTimeout(() => {
+        box.style.backgroundColor = ''; // Reset background color after 1 second
+      }, 1000);
+    });
+  }
+}
+
+// Function to generate a random color
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// Create 800 boxes when the window loads
+window.onload = createBoxes;
